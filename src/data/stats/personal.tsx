ac7a8d55d8adbe979a795a475f8cb4c4a67bd6f2
cloudsofsushi <sushmita7988@gmail.com>
2026-03-05 09:08:08 +0530

    stats

--- src/data/stats/personal.tsx
+++ src/data/stats/personal.tsx
@@ -9,7 +9,7 @@ const Age: React.FC = () => {
 
   const tick = () => {
     const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
-    const birthTime = new Date('1990-02-05T09:24:00');
+    const birthTime = new Date('2001-09-21T09:24:00');
     setAge(((Date.now() - birthTime.getTime()) / divisor).toFixed(11));
   };
 
@@ -24,6 +24,19 @@ const Age: React.FC = () => {
   return <>{age}</>;
 };
 
+const Coffee = () => {
+  const start = new Date('2026-01-01');
+  const end = new Date(); // ✅ FIX
+  let coffee = 2300;
+
+  while (end.getTime() > start.getTime()) {
+    coffee += 1;
+    start.setDate(start.getDate() + 1);
+  }
+
+  return coffee;
+};
+
 const data: StatData[] = [
   {
     key: 'age',
@@ -31,15 +44,49 @@ const data: StatData[] = [
     value: <Age />,
   },
   {
-    key: 'countries',
-    label: 'Countries visited',
-    value: 53,
-    link: 'https://www.google.com/maps/d/embed?mid=1iBBTscqateQ93pWFVfHCUZXoDu8&z=2',
+    key: 'city',
+    label: 'Current city',
+    value: 'Faridabad',
   },
   {
-    key: 'location',
-    label: 'Current city',
-    value: 'New York, NY',
+    key: 'coffee',
+    label: 'Cups of chai/coffee',
+    value: <Coffee />,
+  },
+  {
+    key: 'FavBeverage',
+    label: 'Favorite beverage',
+    value: 'Black Current Shake',
+  },
+  {
+    key: 'MusicHours',
+    label: 'Weekly music hours',
+    value: 18,
+  },
+  {
+    key: 'AvgSleep',
+    label: 'Average sleep per night',
+    value: 6.8,
+  },
+  {
+    key: 'BooksThisYear',
+    label: 'Books read this year',
+    value: '12',
+  },
+  {
+    key: 'TotalBooks',
+    label: 'Total books read',
+    value: 86,
+  },
+  {
+    key: 'Cities',
+    label: 'Cities visited',
+    value: 4,
+  },
+  {
+    key: 'TopDestination',
+    label: 'Travel destination wish',
+    value: 'Switzerland',
   },
 ];
 
--- src/data/stats/site.ts
+++ src/data/stats/site.ts
@@ -4,41 +4,28 @@ import { StatData } from '../../components/Stats/types';
 
 /* Keys match keys returned by the github api. Fields without keys are
  * mostly jokes. To see everything returned by the github api, run:
- curl https://api.github.com/repos/mldangelo/personal-site
+ curl https://api.github.com/repos/cloudsofsushi/cloudsofsushi.github.io
  */
 const data: StatData[] = [
   {
     label: 'Stars this repository has on github',
     key: 'stargazers_count',
-    link: 'https://github.com/mldangelo/personal-site/stargazers',
+    link: 'https://github.com/cloudsofsushi/cloudsofsushi.github.io/stargazers',
   },
   {
     label: 'Number of people watching this repository',
     key: 'subscribers_count',
-    link: 'https://github.com/mldangelo/personal-site/stargazers',
+    link: 'https://github.com/cloudsofsushi/cloudsofsushi.github.io/stargazers',
   },
   {
     label: 'Number of forks',
     key: 'forks',
-    link: 'https://github.com/mldangelo/personal-site/network',
-  },
-  {
-    label: 'Number of spoons',
-    value: '0',
-  },
-  {
-    label: 'Number of linter warnings',
-    value: '0', // enforced via github workflow
-  },
-  {
-    label: 'Open github issues',
-    key: 'open_issues_count',
-    link: 'https://github.com/mldangelo/personal-site/issues',
+    link: 'https://github.com/cloudsofsushi/cloudsofsushi.github.io/network',
   },
   {
     label: 'Last updated at',
     key: 'pushed_at',
-    link: 'https://github.com/mldangelo/personal-site/commits',
+    link: 'https://github.com/cloudsofsushi/cloudsofsushi.github.io/commits',
     format: (x: unknown) => dayjs(x as string).format('MMMM DD, YYYY'),
   },
   {
@@ -46,8 +33,8 @@ const data: StatData[] = [
     /* find . | grep ".js" | grep -vE ".min.js|node_modules|.git|.json" |
     xargs -I file cat file | wc -l */
     label: 'Lines of TypeScript powering this website',
-    value: '2279',
-    link: 'https://github.com/mldangelo/personal-site/graphs/contributors',
+    value: '3000',
+    link: 'https://github.com/cloudsofsushi/cloudsofsushi.github.io/graphs/contributors',
   },
 ];
 
